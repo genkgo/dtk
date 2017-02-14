0984dd0366126b97ae1eebd640952ee9fdb5279d
Frederik Bosch <f.bosch@genkgo.nl>
2017-02-14 12:00:40 +0100

    add production and development builds

--- test/build-test.js
+++ test/build-test.js
@@ -14,7 +14,7 @@ let expectations = path.join(__dirname, 'expectations');
 
 let app;
 
-describe('test build', function() {
+describe('development build', function() {
 
   before(function() {
     process.chdir(__dirname + '/fixtures/build');
@@ -22,6 +22,37 @@ describe('test build', function() {
     app = makeTestHelper({
       subject: function() {
         return new DtkApp({}).build({ 'environment': 'development'});
+      },
+      fixturePath: inputPath
+    });
+  });
+
+
+  afterEach(function () {
+    // return cleanupBuilders();
+  });
+
+  it('build all', function () {
+    return app().then(function(results) {
+      let outputPath = results.directory;
+
+      let outputJs = fs.readFileSync(path.join(outputPath, 'js/scripts.js'), 'utf8');
+      let outputCss = fs.readFileSync(path.join(outputPath, 'css/screen.css'), 'utf8');
+      expect(outputJs.length).to.above(0);
+      expect(outputCss.length).to.above(0);
+    });
+  });
+
+});
+
+describe('production build', function() {
+
+  before(function() {
+    process.chdir(__dirname + '/fixtures/build');
+
+    app = makeTestHelper({
+      subject: function() {
+        return new DtkApp({}).build({ 'environment': 'production'});
       },
       fixturePath: inputPath
     });
